5c62f45941fa24ce6f6de00c99530aaf3be6dc34
Ivan <40150977+goshovsky96@users.noreply.github.com>
2018-10-14 15:22:03 +0300

    ЛР#6 - Опрацювання списків і рядків.
    * ІІI частина
    Заданий рядок, в якому буква h зустрічається мінімум два рази. Виведіть рядок, який отримується  з даного рядка видаленням першого і останнього входження літери h, а також всіх символів, що знаходяться між ними. Вхідні дані: Вводиться рядок.

---
+++ Goshovsky_task6_3.py
@@ -0,0 +1,33 @@
+# Створення прикладних програм на мові Python
+# Лабораторна робота №6_3
+# Гошовський Іван, №6
+def search_ind(text):
+	"""Шукає індекс першої ind[0] та останньої ind[1] літери h (H)
+		Якщо літера h (H) в рядку одна - то ind[0] = ind[1]
+		Якщо в рядку немає літери h (H) - то ind[0] = len(text), ind[1] = 0
+	"""
+	ind = [0,len(text)]
+	for t in text:
+		if (t == 'h' or t == 'H'):
+			break 
+		ind[0] += 1
+
+	for i in range(len(text)):
+		if text[len(text)-i-1] == 'h' or text[len(text)-i-1] == 'H':
+			break
+		ind[1] = len(text)-i-2
+	return ind
+
+def out_text(text:str,ind:list):
+	if ind[0] == len(text) and ind[1] == -1:
+		return(text)
+	elif ind[0] == ind[1]:
+		return(text[0:ind[0]])
+	else:
+		return(text[0:ind[0]]+text[ind[1]+1:]) 
+s = input('Введіть текст:')
+index = search_ind(s)
+
+print('Кількість літер в рядку:',len(s))
+print('Індекси: 1 літера - {}, остання - {}'.format(index[0],index[1]))
+print('Результат:',out_text(s,index))
