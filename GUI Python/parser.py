f9a16adf686b26bad891c192dac34106f4c589e1
Ivan <40150977+goshovsky96@users.noreply.github.com>
2018-11-30 11:58:15 +0200

    Add files via upload

---
+++ GUI Python/parser.py
@@ -0,0 +1,26 @@
+import requests
+from datetime import date
+dict_room = {1: set(), 2: set(), 3: set(), 4: set(), 5: set(), 6: set(), 7: set()}
+sdate = "01.09.2018"
+edate = "30.11.2018"
+teacher = "Савка (п) Іван Ярославович"
+group = "ПММ-2"
+url = "http://asu.pnu.edu.ua/cgi-bin/timetable.cgi"
+headers = {'Content-Type': 'text/html; charset=windows-1251'}
+data = {'teacher': teacher.encode('cp1251'), 'sdate': sdate, 'edate': edate}
+r = requests.post(url, headers=headers, data = data )
+r.encoding = 'cp1251'
+soup = BeautifulSoup(r.text, "html.parser")
+rows = soup.find_all('tr')
+dict_line = {}
+if rows:
+    for row in rows:
+        cols = row.find_all('td')
+        if cols[2].text != '':
+            if (len(cols[2].text.split()) != 0):
+                key = cols[2].text.split()[-1] 
+                if (key in dict_line.keys()):
+                    dict_line[key]+=1
+                else: 
+                    dict_line[key] = 1
+print(dict_line)
